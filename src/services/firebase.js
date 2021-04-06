09a12d0a1a95ea88b36b3e1fc0567d98a8197cd1
heyputnam <heyputnam@gmail.com>
2021-04-06 14:57:08 -0400

    adding firebase google auth

---
+++ src/services/firebase.js
@@ -0,0 +1,25 @@
+//adding auth to dev skills
+
+//import the firebase core module 
+
+
+//import auth package from firebase (special package)
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOQYNsdd7_pjTS1JNOZxxY5ZBSasD6TqI",
+    authDomain: "react-dev-skills-d8803.firebaseapp.com",
+    projectId: "react-dev-skills-d8803",
+    storageBucket: "react-dev-skills-d8803.appspot.com",
+    messagingSenderId: "608824427906",
+    appId: "1:608824427906:web:d0c57b125f823c75923b9b"
+  };
+
+  // initilize the firebase app
+  
+  
+  //set up firebase provider
+
+  //set auth actions (login/logout)
+
+
+  //export the actions (export default)
